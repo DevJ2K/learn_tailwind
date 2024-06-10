a071488daa1952d5fc860b981c1bfc64b40261f1
DevJ2K <ajavontheopro@gmail.com>
2024-06-10 14:10:56 +0200

    Learning colors scheme...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,17 @@
 <template>
 
-  <main class="w-full h-screen flex justify-center items-center flex-col">
-    <p class="bg-red-500 p-8">Salut</p>
-    <p class="bg-red-500 p-8">Salut</p>
+  <main class="w-full h-screen bg-blue-100 flex justify-center items-center flex-col">
+    <!-- <p class="bg-red-500 p-8">Salut</p>
+    <p class="bg-red-500 p-8">Salut</p> -->
+    <!-- Create a simple card -->
+    <div class="bg-blue-50 text-blue-950 rounded-md shadow-lg p-8  border border-blue-300 m-4 font-bold">
+      <h1>My card</h1>
+    </div>
   </main>
 
-  <div>
+  <!-- <div>
     <button class="btn-primary">Bouton</button>
-  </div>
+  </div> -->
 </template>
 
 <script setup>
