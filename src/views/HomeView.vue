f93bc83207432e3f85909a6f1fb4d632a4823c4a
DevJ2K <ajavontheopro@gmail.com>
2024-06-10 14:28:12 +0200

    saving...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,12 @@
     <!-- <p class="bg-red-500 p-8">Salut</p>
     <p class="bg-red-500 p-8">Salut</p> -->
     <!-- Create a simple card -->
-    <div class="bg-blue-50 text-blue-950 rounded-md shadow-lg p-8  border border-blue-300 m-4 font-bold">
-      <h1>My card</h1>
+    <div class="bg-blue-50 text-blue-950 rounded-md shadow-lg p-8 border border-blue-300 m-4 flex flex-col justify-start max-w-md w-full gap-6">
+      <h1 class="font-bold text-2xl">My card</h1>
+      <p class="text-sm/6 font-medium">
+        In enim culpa mollit ipsum enim consectetur ad sunt elit esse reprehenderit ullamco duis voluptate. Velit voluptate qui elit officia. Exercitation magna ut duis mollit cupidatat aute veniam aliqua et eiusmod. Amet exercitation laboris voluptate fugiat ut ex esse consequat laborum exercitation qui voluptate laborum ex. Enim ad Lorem nostrud dolor tempor est aliquip eu nisi eiusmod pariatur labore laboris.
+      </p>
+      <button class="rounded-md bg-blue-500 px-4 py-2 shadow-lg text-blue-50 font-bold">Click me !</button>
     </div>
   </main>
 
