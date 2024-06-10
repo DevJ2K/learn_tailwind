9a7fc4919d2407a51818976828362620e490d667
DevJ2K <ajavontheopro@gmail.com>
2024-06-10 17:05:14 +0200

    saving...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,7 @@
       <p class="text-sm/6 font-medium">
         In enim culpa mollit ipsum enim consectetur ad sunt elit esse reprehenderit ullamco duis voluptate. Velit voluptate qui elit officia. Exercitation magna ut duis mollit cupidatat aute veniam aliqua et eiusmod. Amet exercitation laboris voluptate fugiat ut ex esse consequat laborum exercitation qui voluptate laborum ex. Enim ad Lorem nostrud dolor tempor est aliquip eu nisi eiusmod pariatur labore laboris.
       </p>
-      <button class="rounded-md bg-blue-500 px-4 py-2 shadow-lg text-blue-50 font-bold">Click me !</button>
+      <button class="rounded-md bg-blue-500 px-4 py-2 shadow-lg text-blue-50 font-bold hover:bg-blue-600 focus:ring-2 ring-blue-300 focus:outline-2 outline-white active:bg-blue-700 ring-offset-2">Click me !</button>
     </div>
   </main>
 
