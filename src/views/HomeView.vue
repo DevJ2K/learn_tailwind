27c0885ebae5d57a95bd3130bfd8a3a10fa22404
DevJ2K <ajavontheopro@gmail.com>
2024-06-11 13:11:57 +0200

    Add Eslint

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,23 @@
 <template>
 
-  <main class="w-full h-screen bg-blue-100 flex justify-center items-center flex-col">
+  <main class="flex h-screen w-full flex-col items-center justify-center bg-blue-100">
     <!-- <p class="bg-red-500 p-8">Salut</p>
     <p class="bg-red-500 p-8">Salut</p> -->
     <!-- Create a simple card -->
-    <div class="bg-blue-50 text-blue-950 rounded-md shadow-lg p-8 border border-blue-300 m-4 flex flex-col justify-start max-w-md w-full gap-6">
-      <h1 class="font-bold text-2xl">My card</h1>
+    <div class="m-4 flex w-full max-w-md flex-col justify-start gap-6 rounded-md border border-blue-300 bg-blue-50 p-8 text-blue-950 shadow-lg">
+      <h1 class="text-2xl font-bold">My card</h1>
       <p class="text-sm/6 font-medium">
         In enim culpa mollit ipsum enim consectetur ad sunt elit esse reprehenderit ullamco duis voluptate. Velit voluptate qui elit officia. Exercitation magna ut duis mollit cupidatat aute veniam aliqua et eiusmod. Amet exercitation laboris voluptate fugiat ut ex esse consequat laborum exercitation qui voluptate laborum ex. Enim ad Lorem nostrud dolor tempor est aliquip eu nisi eiusmod pariatur labore laboris.
       </p>
-      <ul class="justify-center max-w-md w-full grid gap-2 grid-cols-2 sm:grid-cols-1">
-        <li class="bg-blue-200 flex justify-center rounded-md shadow-sm text-blue-950 font-semibold px-4 py-2">Item 1</li>
-        <li class="bg-blue-200 flex justify-center rounded-md shadow-sm text-blue-950 font-semibold px-4 py-2">Item 2</li>
-        <li class="bg-blue-200 flex justify-center rounded-md shadow-sm text-blue-950 font-semibold px-4 py-2">Item 3</li>
-        <li class="bg-blue-200 flex justify-center rounded-md shadow-sm text-blue-950 font-semibold px-4 py-2">Item 4</li>
+      <ul class="grid w-full max-w-md grid-cols-2 justify-center gap-2 sm:grid-cols-1">
+        <li class="flex justify-center rounded-md bg-blue-200 px-4 py-2 font-semibold text-blue-950 shadow-sm">Item 1</li>
+        <li class="flex justify-center rounded-md bg-blue-200 px-4 py-2 font-semibold text-blue-950 shadow-sm">Item 2</li>
+        <li class="flex justify-center rounded-md bg-blue-200 px-4 py-2 font-semibold text-blue-950 shadow-sm">Item 3</li>
+        <li class="flex justify-center rounded-md bg-blue-200 px-4 py-2 font-semibold text-blue-950 shadow-sm">Item 4</li>
       </ul>
-      <div class="flex max-md:flex-col gap-2 justify-center">
-        <button class="rounded-md bg-blue-500 px-4 py-2 shadow-lg text-blue-50 font-bold hover:bg-blue-600 focus:ring-2 ring-blue-300 focus:outline-2 outline-white active:bg-blue-700 ring-offset-2">Click me !</button>
-        <button class="rounded-md bg-green-500 px-4 py-2 shadow-lg text-green-50 font-bold hover:bg-green-600 focus:ring-2 ring-green-300 focus:outline-2 outline-white active:bg-green-700 ring-offset-2">Click me !</button>
+      <div class="flex justify-center gap-2 max-md:flex-col">
+        <button class="rounded-md bg-blue-500 px-4 py-2 font-bold text-blue-50 shadow-lg outline-white ring-blue-300 ring-offset-2 hover:bg-blue-600 focus:outline-2 focus:ring-2 active:bg-blue-700">Click me !</button>
+        <button class="rounded-md bg-green-500 px-4 py-2 font-bold text-green-50 shadow-lg outline-white ring-green-300 ring-offset-2 hover:bg-green-600 focus:outline-2 focus:ring-2 active:bg-green-700">Click me !</button>
       </div>
     </div>
   </main>
