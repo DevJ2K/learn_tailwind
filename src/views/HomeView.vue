7fafeaa1765a736032f21385280e535e7fbc4647
DevJ2K <ajavontheopro@gmail.com>
2024-06-11 12:47:10 +0200

    PENDING : Adding check breakpoint [max-x:yyy; x: yyy]

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,16 @@
       <p class="text-sm/6 font-medium">
         In enim culpa mollit ipsum enim consectetur ad sunt elit esse reprehenderit ullamco duis voluptate. Velit voluptate qui elit officia. Exercitation magna ut duis mollit cupidatat aute veniam aliqua et eiusmod. Amet exercitation laboris voluptate fugiat ut ex esse consequat laborum exercitation qui voluptate laborum ex. Enim ad Lorem nostrud dolor tempor est aliquip eu nisi eiusmod pariatur labore laboris.
       </p>
-      <button class="rounded-md bg-blue-500 px-4 py-2 shadow-lg text-blue-50 font-bold hover:bg-blue-600 focus:ring-2 ring-blue-300 focus:outline-2 outline-white active:bg-blue-700 ring-offset-2">Click me !</button>
+      <ul class="justify-center max-w-md w-full grid gap-2 grid-cols-2 sm:grid-cols-1">
+        <li class="bg-blue-200 flex justify-center rounded-md shadow-sm text-blue-950 font-semibold px-4 py-2">Item 1</li>
+        <li class="bg-blue-200 flex justify-center rounded-md shadow-sm text-blue-950 font-semibold px-4 py-2">Item 2</li>
+        <li class="bg-blue-200 flex justify-center rounded-md shadow-sm text-blue-950 font-semibold px-4 py-2">Item 3</li>
+        <li class="bg-blue-200 flex justify-center rounded-md shadow-sm text-blue-950 font-semibold px-4 py-2">Item 4</li>
+      </ul>
+      <div class="flex max-md:flex-col gap-2 justify-center">
+        <button class="rounded-md bg-blue-500 px-4 py-2 shadow-lg text-blue-50 font-bold hover:bg-blue-600 focus:ring-2 ring-blue-300 focus:outline-2 outline-white active:bg-blue-700 ring-offset-2">Click me !</button>
+        <button class="rounded-md bg-green-500 px-4 py-2 shadow-lg text-green-50 font-bold hover:bg-green-600 focus:ring-2 ring-green-300 focus:outline-2 outline-white active:bg-green-700 ring-offset-2">Click me !</button>
+      </div>
     </div>
   </main>
 
