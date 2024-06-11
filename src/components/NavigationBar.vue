e0ddb15204ee5f3428d672018708926dbee1ad43
DevJ2K <ajavontheopro@gmail.com>
2024-06-11 17:18:45 +0200

    Adding more styles...

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,10 +1,10 @@
 <template>
-	<div class="absolute top-0 flex w-full flex-row justify-between bg-blue-300 px-16 py-10">
+	<div class="absolute top-0 flex w-full flex-row justify-between bg-blue-300 px-16 py-10 shadow-lg">
 		<div>
 
 			<h1>DevJ2K</h1>
 		</div>
-		<div class="hidden flex-row gap-8 lg:flex">
+		<div class="hidden flex-row gap-8 lg:flex font-bold">
 			<h3>About</h3>
 			<h3>Skills</h3>
 			<h3>Projects</h3>
